46770d87c6bff927f147d1c8fd60aad029efdee3
Emily Evenden <54719919+eevenden@users.noreply.github.com>
2020-11-11 16:08:33 -0500

    update dataset file

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,13 @@
-## code to prepare `DATASET` dataset goes here
+#' Production and farm value of maple products in Canada
+#'
+#' @source South Africa's country boundary provided by the Humanitarian Data Exchange 
+#' \url{https://data.humdata.org/dataset/south-africa-admin-level-1-boundaries}
+#' @format An ESRI shapefile:
+#' \describe{
+#'  \item{Year}{A value between 1924 and 2015.}
+#' @examples
+#' \dontrun{
+#'  
+#' }
+"SouthAfrica_Boundary"
 
-usethis::use_data(DATASET, overwrite = TRUE)
