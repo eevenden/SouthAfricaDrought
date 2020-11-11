d68c36d0e2a2d9d97c3a9f325c9afe499a50061f
Emily Evenden <54719919+eevenden@users.noreply.github.com>
2020-11-11 12:52:25 -0500

    Add files via upload

---
+++ R/avg_stack.R
@@ -0,0 +1,25 @@
+#' Avg_stack function
+#'
+#' @description This function allows you to search for raster files in a folder, crop them, average them, and output the result as a new file
+#' @param x folder where data files are
+#' @param y the pattern by which to search for files
+#' @param boundary the vector file used to crop each raster file retrieved 
+#' @param aa output file name
+#' @export
+#' @examples
+#' monthly_temp("C:/Users/Emily/Documents/Fall_2020/Geospatial_R/MODIS_Temp", "2020_01.*\\.tif", SA, "2020_01_mean_temp.tif")
+#' 
+avg_stack <- function(x, y, boundary, aa){
+  
+  fs <- dir(x, pattern = y, full.names = TRUE)
+  
+  s <- fs %>% lapply(raster) %>% stack
+  
+  boundary <- st_transform(boundary, crs = st_crs(s[[1]]))
+  
+  new_s <- crop(s, boundary)
+  
+  temp_zmu <- calc(x = new_s, fun = mean)
+  
+  writeRaster(temp_zmu, filename = file.path(x, aa))
+}
