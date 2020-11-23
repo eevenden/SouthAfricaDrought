71b15fd3d8d882cbc111ad4760067945497db3f7
Emily Evenden <54719919+eevenden@users.noreply.github.com>
2020-11-23 13:33:21 -0500

    add long term rainfall data

--- Extra_Scripts/Monthly_MODIS_Creation.R
+++ Extra_Scripts/Monthly_MODIS_Creation.R
@@ -4,21 +4,17 @@ library(sf)
 library(dplyr)
 
 
-SA <- read_sf(dsn = "C:/Users/Emily/Documents/Fall_2020/Geospatial_R/SouthAfricaDrought", layer = "SouthAfrica_Boundary")
-crs(SA)
+SA_new <- system.file("extdata/SouthAfrica_Boundary.shp", package = "SouthAfricaDrought") %>% st_read()
+SA_new
 
-fs <- dir("C:/Users/Emily/Documents/Fall_2020/Geospatial_R/Chirps", pattern = "*Prec*", full.names = TRUE)
+fs <- dir("C:/Users/Emily/Documents/Fall_2020/Geospatial_R/Chirps/LT", pattern = "*LT.*\\.tif", full.names = TRUE)
+fs
 
 l <- lapply(fs, raster)
+plot(l[[1]])
 
-month1 <- crop(l[[1]], SA)
-month2 <- crop(l[[2]], SA)
-month3 <- crop(l[[3]], SA)
-month4 <- crop(l[[4]], SA)
-month5 <- crop(l[[5]], SA)
+lt_stack <- stack(lapply(fs, function(x) crop(raster(x), SA_new)))
 
-s <- stack(c(month1, month2, month3, month4, month5))
-  
-names(s) <- c("2019_11_mean_celsius", "2019_12_mean_celsius", "2020_01_mean_celsius", "2020_02_mean_celsius", "2020_03_mean_celsius")
+names(s) <- c("Nov_LT", "Dec_LT", "Jan_LT", "Feb_LT", "March_LT")
 
-writeRaster(s, "SA_MODIS_Monthly_Mean_Temp", format = "GTiff")
+writeRaster(s, "SA_LT_Mean_Precip", format = "GTiff")
--- Extra_Scripts/Weekly_Chirps_Creation.R
+++ Extra_Scripts/Weekly_Chirps_Creation.R
@@ -10,33 +10,7 @@ SA <- system.file("extdata/SouthAfrica_Boundary.shp",
 fs <- dir("C:/Users/Emily/Documents/Fall_2020/Geospatial_R/Chirps/SouthAfrica_Imagery/SouthAfrica_Imagery", pattern = "SA.*\\.tif", full.names = TRUE)
 fs
 
-l <- lapply(fs, raster)
-
-week1 <- crop(l[[1]], SA)
-week2 <- crop(l[[2]], SA)
-week3 <- crop(l[[3]], SA)
-week4 <- crop(l[[4]], SA)
-week5 <- crop(l[[5]], SA)
-week6 <- crop(l[[6]], SA)
-week7 <- crop(l[[7]], SA)
-week8 <- crop(l[[8]], SA)
-week9 <- crop(l[[9]], SA)
-week10 <- crop(l[[10]], SA)
-week11 <- crop(l[[11]], SA)
-week12 <- crop(l[[12]], SA)
-week13 <- crop(l[[13]], SA)
-week14 <- crop(l[[14]], SA)
-week15 <- crop(l[[15]], SA)
-week16 <- crop(l[[16]], SA)
-week17 <- crop(l[[17]], SA)
-week18 <- crop(l[[18]], SA)
-week19 <- crop(l[[19]], SA)
-week20 <- crop(l[[20]], SA)
-week21 <- crop(l[[21]], SA)
-week22 <- crop(l[[22]], SA)
-week23 <- crop(l[[23]], SA)
-
-weekly_s <- raster::stack(week1, week2, week3, week4, week5, week6, week7, week8, week9, week10, week11, week12, week13, week14, week15, week16, week17, week18, week19, week20, week21, week22, week23)
+weekly_s <- stack(lapply(fs, function(x) crop(raster(x), SA)))
 
 names(weekly_s) <- c('week1_2019-10-27_2019-11-02',
               'week2_2019-11-17_2019-11-23',
