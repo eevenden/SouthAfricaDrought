c17fafd13b7659d953e2c2771af3be92868c1a22
Emily Evenden <54719919+eevenden@users.noreply.github.com>
2020-11-23 14:18:37 -0500

    begin drought indices functions

---
+++ R/noaa_drought.R
@@ -0,0 +1,16 @@
+#' noaa_drought function
+#'
+#' @description 
+#' @param x rasterstack containing precipitation data
+#' @export
+#' @examples
+#' avg_stack("C:/Users/Emily/Documents/Fall_2020/Geospatial_R/MODIS_Temp", "2020_01.*\\.tif", SA, "2020_01_mean_temp.tif")
+#'
+noaa_drought <- function(x){
+#quantile(x, c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1), na.rm=TRUE) #get numerical range for each decile
+
+#reclass each layer of the stack according to ranges, save as new raster layer
+
+#return new stack
+
+}
