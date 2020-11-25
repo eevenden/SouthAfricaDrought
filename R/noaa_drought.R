0e72b033243b6c166e2bcfed96f069f343ad0f5f
Emily Evenden <54719919+eevenden@users.noreply.github.com>
2020-11-25 09:58:44 -0500

    finalize noaa_drought function

--- R/noaa_drought.R
+++ R/noaa_drought.R
@@ -2,10 +2,10 @@
 #'
 #' @description 
 #' @param weekly_prec rasterstack containing weekly precipitation data
-#' @param list the name of a list creatd outside of this function
+#' @param list the name of a list created outside of this function
 #' @export
 #' @examples
-#' avg_stack("C:/Users/Emily/Documents/Fall_2020/Geospatial_R/MODIS_Temp", "2020_01.*\\.tif", SA, "2020_01_mean_temp.tif")
+#' noaa_drought(weekly_mean_precip, new_list)
 #'
 noaa_drought <- function(weekly_prec, list){
   
@@ -17,13 +17,13 @@ noaa_drought <- function(weekly_prec, list){
     
     mean_eightweek <- calc(eightweek, fun = mean)
     
-    noaa <- (present/mean_eightweek)
+    noaa <- (present / mean_eightweek)
     plot(noaa)
     
     list[[i]] <- noaa
     
-    i = i+1
-    x = x+1
+    i = i + 1
+    x = x + 1
   }
 
 }
